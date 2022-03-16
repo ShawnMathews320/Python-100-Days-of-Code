79e933200865d69f27d5dba88c5ba58c3fd1061c
shawn.mathews <shawn.mathews@stmartin.edu>
2022-03-15 17:16:01 -0700

    complete solution

---
+++ bandNameGenerator/program.py
@@ -0,0 +1,5 @@
+print ("Welcome to the Band Name Generator \nWhat's the name of the city you grew up in?")
+userCityName = input()  # get user input
+print ("What's your pet's name?")
+userPetName = input()  # get user input
+print ("Your band name could be " + userCityName + " " + userPetName)  # string concatenation
